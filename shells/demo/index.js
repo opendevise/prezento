02955ecb16a39b3bac60ddd97b2929fa9272d37d
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2013-10-27 16:42:55 +0100

    refactor(demo-shell): refactor index.js scopes and variables, better styling of fullscreen mode

--- shells/demo/index.js
+++ shells/demo/index.js
@@ -1,116 +1,124 @@
-'use strict';
+/*global prezento: false */
 
-var searchParams = (function () {
-  var params = /^\?(.*?=.*?)(?:&(.*?=.*?))*$/.exec(location.search),
-      paramsObj = {};
+(function () {
+  'use strict';
 
-  for (var i = 1; params[i]; i++) {
-    var keyValue = params[i].split('=');
-    paramsObj[keyValue[0]] = keyValue[1];
-  }
+  // Helpers
 
-  return paramsObj;
-})();
+  var getSearchParams = function () {
+    var params = /^\?(.*?=.*?)(?:&(.*?=.*?))*$/.exec(location.search),
+        paramsObj = {};
 
-var $ = (function () {
-  var cache = {};
-
-  return function (selector) {
-    if (!cache[selector]) {
-      cache[selector] = document.querySelector(selector);
+    for (var i = 1; params[i]; i++) {
+      var keyValue = params[i].split('=');
+      paramsObj[keyValue[0]] = keyValue[1];
     }
 
-    return cache[selector];
+    return paramsObj;
   };
-})();
 
-$('iframe').src = searchParams.url;
-//var popup = window.open(searchParams.url);
+  var $ = (function () {
+    var cache = {};
 
-var steps;
+    return function (selector) {
+      if (!cache[selector]) {
+        cache[selector] = document.querySelector(selector);
+      }
 
-var configListeners = {
+      return cache[selector];
+    };
+  })();
 
-  ready: function (slideDeckInfos) {
+  var addClickListener = function (selector, callback) {
+    $(selector).addEventListener('click', function () {
+      callback();
+    }, false);
+  };
 
-    $('.title').innerHTML = slideDeckInfos.title;
-    $('.author').innerHTML = slideDeckInfos.metas.author || '';
-    $('.description').innerHTML = slideDeckInfos.metas.description || '';
+  // Setup the slide deck iframe
+  $('iframe').src = getSearchParams().url;
 
-    steps = slideDeckInfos.steps;
-    $('.steps-count').innerHTML = steps.length;
+  // Setup the slide deck proxy
 
-    $('.features').innerHTML = slideDeckInfos.features.join(', ') || 'none';
+  var steps;
 
-    if (slideDeckInfos.features.indexOf('multimedia') !== -1) {
-      $('[data-action="toggleMultimedia"]').disabled = false;
+  var slideDeck = prezento.createSlideDeckProxy('iframe')
+
+      .configListeners({
+
+        ready: function (slideDeckInfos) {
+
+          $('.title').innerHTML = slideDeckInfos.title;
+          $('.author').innerHTML = slideDeckInfos.metas.author || '';
+          $('.description').innerHTML = slideDeckInfos.metas.description || '';
+
+          steps = slideDeckInfos.steps;
+          $('.steps-count').innerHTML = steps.length;
+
+          $('.features').innerHTML = slideDeckInfos.features.join(', ') || 'none';
+
+          if (slideDeckInfos.features.indexOf('multimedia') !== -1) {
+            $('[data-action="toggleMultimedia"]').disabled = false;
+          }
+
+          if (slideDeckInfos.features.indexOf('overview') !== -1) {
+            $('[data-action="toggleOverview"]').disabled = false;
+          }
+        },
+
+        cursor: function (cursor) {
+          $('.current-cursor').value = cursor;
+        },
+
+        step: function (step) {
+          $('.current-step').innerHTML = steps.indexOf(step) + 1;
+        },
+
+        notes: function (notes) {
+          $('.current-notes').innerHTML = notes;
+        }
+      });
+
+  // Setup action calls
+
+  addClickListener('[data-action="goTo"]', function () {
+    slideDeck.goTo($('.current-cursor').value);
+  });
+
+  addClickListener('[data-action="first"]', slideDeck.first);
+  addClickListener('[data-action="prev"]', slideDeck.prev);
+  addClickListener('[data-action="next"]', slideDeck.next);
+  addClickListener('[data-action="last"]', slideDeck.last);
+  addClickListener('[data-action="toggleMultimedia"]', slideDeck.toggleMultimedia);
+  addClickListener('[data-action="toggleOverview"]', slideDeck.toggleOverview);
+
+  addClickListener('[data-action="goFullscreen"]', function () {
+    var requestFullscreen = $('.screen').requestFullscreen || $('.screen').requestFullScreen || $('.screen').mozRequestFullScreen || $('.screen').webkitRequestFullScreen;
+    if (requestFullscreen) {
+      requestFullscreen.apply($('.screen'));
     }
+  });
 
-    if (slideDeckInfos.features.indexOf('overview') !== -1) {
-      $('[data-action="toggleOverview"]').disabled = false;
+  // Fullscreen stuffs
+
+  var toggleFullscreen = function (element) {
+    if (element != null) {
+      $('.screen').classList.add('full');
+    } else {
+      $('.screen').classList.remove('full');
     }
-  },
+  };
 
-  cursor: function (cursor) {
-    $('.current-cursor').value = cursor;
-  },
-
-  step: function (step) {
-    $('.current-step').innerHTML = steps.indexOf(step) + 1;
-  },
-
-  notes: function (notes) {
-    $('.current-notes').innerHTML = notes;
-  }
-};
-
-var slideDeck = prezento.createSlideDeckProxy('iframe')
-    .configListeners(configListeners);
-
-//var popupSlideDeck = prezento.createSlideDeckProxy(popup)
-//    .configListeners(configListeners);
-
-
-var addClickListener = function (selector, callback) {
-  $(selector).addEventListener('click', function () {
-    callback();
+  document.addEventListener('mozfullscreenchange', function () {
+    toggleFullscreen(document.mozFullscreenElement);
   }, false);
-};
 
-addClickListener('[data-action="first"]', slideDeck.first);
-addClickListener('[data-action="prev"]', slideDeck.prev);
-addClickListener('[data-action="next"]', slideDeck.next);
-addClickListener('[data-action="last"]', slideDeck.last);
-addClickListener('[data-action="toggleMultimedia"]', slideDeck.toggleMultimedia);
-addClickListener('[data-action="toggleOverview"]', slideDeck.toggleOverview);
+  document.addEventListener('webkitfullscreenchange', function () {
+    toggleFullscreen(document.webkitFullscreenElement);
+  }, false);
 
-addClickListener('[data-action="goTo"]', function () {
-  slideDeck.goTo($('.current-cursor').value);
-});
+  document.addEventListener('fullscreenchange', function () {
+    toggleFullscreen(document.fullscreenElement);
+  }, false);
 
-addClickListener('[data-action="goFullscreen"]', function () {
-  var requestFullscreen = $('.screen').requestFullscreen || $('.screen').requestFullScreen || $('.screen').mozRequestFullScreen || $('.screen').webkitRequestFullScreen;
-  if (requestFullscreen) {
-    requestFullscreen.apply($('.screen'));
-  }
-});
-
-var toggleFullscreen = function (element) {
-  if (element != null) {
-    $('.screen').classList.add('full');
-  } else {
-    $('.screen').classList.remove('full');
-  }
-};
-
-document.addEventListener("mozfullscreenchange", function (e) {
-  toggleFullscreen(document.mozFullscreenElement);
-}, false);
-
-document.addEventListener("webkitfullscreenchange", function (e) {
-  toggleFullscreen(document.webkitFullscreenElement);
-}, false);
-
-document.addEventListener("fullscreenchange", function (e) {
-  toggleFullscreen(document.fullscreenElement);
-}, false);
+})();
